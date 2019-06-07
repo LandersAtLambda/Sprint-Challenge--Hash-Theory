a08c99d159024e73004a7b3c815bcb00dc6b4251
Mike Landers <landers.mike@gmail.com>
2019-06-07 10:51:09 -0700

    hash project 2 passing

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,17 +6,48 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
+  Answer *result = malloc(sizeof(Answer));
+  ;
+  //insert weights into hash table as the key and index as the value
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, weights[i], i);
+  }
+  for (int i = 0; i < length; i++)
+  {
+    int num = limit - weights[i];
+    int index = hash_table_retrieve(ht, num);
 
-  /* YOUR CODE HERE */
-
+    if ((weights[i] + weights[index]) == limit)
+    {
+      // printf("Limit = Current Index + Diff\n");
+      // printf("%d = %d + %d\n", limit, weights[i], weights[index]);
+      // printf("%d = %d + %d\n", limit, i, index);
+      if (weights[i] >= weights[index])
+      {
+        result->index_1 = i;
+        result->index_2 = index;
+        // printf("%d, %d\n", result->index_1, result->index_2);
+        print_answer(result);
+        free(result);
+        destroy_hash_table(ht);
+      }
+    }
+  }
+  print_answer(NULL);
+  destroy_hash_table(ht);
+  free(result);
   return NULL;
 }
 
 void print_answer(Answer *answer)
 {
-  if (answer != NULL) {
+  if (answer != NULL)
+  {
     printf("%d %d\n", answer->index_1, answer->index_2);
-  } else {
+  }
+  else
+  {
     printf("NULL\n");
   }
 }
@@ -24,25 +55,29 @@ void print_answer(Answer *answer)
 #ifndef TESTING
 int main(void)
 {
-  // TEST 1
-  int weights_1 = {9};
-  Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
-  print_answer(answer_1);  // NULL
+  // // TEST 1
+  // int weights_1 = {9};
+  // Answer *answer_1 = get_indices_of_item_weights(&weights_1, 1, 9);
+  // printf("Test 1: ");
+  // print_answer(answer_1); // NULL
 
-  // TEST 2
-  int weights_2[] = {4, 4};
-  Answer* answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
-  print_answer(answer_2);  // {1, 0}
+  // // TEST 2
+  // int weights_2[] = {4, 4};
+  // Answer *answer_2 = get_indices_of_item_weights(weights_2, 2, 8);
+  // printf("Test 2: ");
+  // print_answer(answer_2); // {1, 0}
 
-  // TEST 3
-  int weights_3[] = {4, 6, 10, 15, 16};
-  Answer* answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
-  print_answer(answer_3);  // {3, 1}
+  // // TEST 3
+  // int weights_3[] = {4, 6, 10, 15, 16};
+  // Answer *answer_3 = get_indices_of_item_weights(weights_3, 5, 21);
+  // printf("Test 3: ");
+  // print_answer(answer_3); // {3, 1}
 
-  // TEST 4
-  int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
-  Answer* answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
-  print_answer(answer_4);  // {6, 2}
+  // // TEST 4
+  // int weights_4[] = {12, 6, 7, 14, 19, 3, 0, 25, 40};
+  // Answer *answer_4 = get_indices_of_item_weights(weights_4, 9, 7);
+  // printf("Test 4: ");
+  // print_answer(answer_4); // {6, 2}
 
   return 0;
 }
--- hash-tables/ex2/ex2.c
+++ hash-tables/ex2/ex2.c
@@ -8,20 +8,39 @@ char **reconstruct_trip(Ticket **tickets, int length)
 {
   HashTable *ht = create_hash_table(length);
   char **route = malloc(length * sizeof(char *));
+  int index = 0;
 
-  /* YOUR CODE HERE */
+  for (int i = 0; i < length; i++)
+  {
+    hash_table_insert(ht, tickets[i]->source, tickets[i]->destination);
+  }
 
+  // sets temp to the destination of the first ticket
+  char *temp = hash_table_retrieve(ht, "NONE");
+
+  while (index < length && strcmp(temp, "None") != 0)
+  {
+    // sets the route at index to the destination that doesn't equal NONE
+    route[index] = temp;
+    index++;
+    temp = hash_table_retrieve(ht, temp);
+  }
+  // sets final route indes to NONE
+  route[index] = "NONE";
+  // Destroy hash table
+  destroy_hash_table(ht);
+  free(route);
+  // Returns route
   return route;
 }
 
 void print_route(char **route, int length)
 {
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     printf("%s\n", route[i]);
   }
 }
-
-
 
 #ifndef TESTING
 int main(void)
@@ -46,7 +65,8 @@ int main(void)
 
   print_route(reconstruct_trip(tickets, 3), 3); // PDX, DCA, NONE
 
-  for (int i = 0; i < 3; i++) {
+  for (int i = 0; i < 3; i++)
+  {
     free(tickets[i]);
   }
 
